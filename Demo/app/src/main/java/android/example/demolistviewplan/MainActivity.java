47b5f814c97293d3ac4cbe938ffbe3cd7a53c50b
LeeShangCyber <lesang541191309@gmail.com>
2020-05-14 10:16:53 +0700

    demo DatePicker

--- Demo/app/src/main/java/android/example/demolistviewplan/MainActivity.java
+++ Demo/app/src/main/java/android/example/demolistviewplan/MainActivity.java
@@ -2,11 +2,13 @@ package android.example.demolistviewplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> mWorkTitle = new ArrayList<String>();
     private WorkAdapter adapter;
 
+    private TextView txtDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtDate = (TextView)findViewById(R.id.txtDate);
 
         adapter = new WorkAdapter(this, mWorkHour, mWorkTitle);
         listView = (ListView)findViewById(R.id.listWork);
@@ -60,6 +66,22 @@ public class MainActivity extends AppCompatActivity {
         mWorkHour.add(hour);
 
         mWorkTitle.add(title);
+
+    }
+
+    public void chooseDate(View view)
+    {
+        DialogFragment dateFragment = new datePicker();
+        dateFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = ( day_string + "/" + month_string + "/" + year_string);
+
+        txtDate.setText(dateMessage);
 
     }
 
